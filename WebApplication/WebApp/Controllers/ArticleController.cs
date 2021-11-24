bdd49bec6516c50e7f4dc0f0c8a050c191f2f3fa
DudeWithAShortName <stefan_kaloqn@abv.bg>
2021-11-24 15:56:43 +0200

    upload

---
+++ WebApplication/WebApp/Controllers/ArticleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult ById(string name)
+        {
+            var viewModel = new DataViewModel();
+            viewModel.ParameterValue = name;
+            this.ViewBag.Name = "Asen";
+            return this.View(viewModel);
+        }
+    }
+}
